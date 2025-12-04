af5f421df09b07116e34edfc82ec6f649015e898
Swapnil Mhaske <swapnil.mhaske@novatiq.com>
2025-12-04 17:52:03 +0530

    feat(urlParam): new locale urlparam embedding to setup language for traductions #31646

---
+++ superset-frontend/src/dashboard/customTrads.ts
@@ -0,0 +1,17 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the "License");
+ */
+import { t } from '@superset-ui/core';
+
+// list of translatable titles
+t('Example');
+
+// list of dataset labels
+
+// list of tab names
+
+// list of presentation texts and link to glossary
